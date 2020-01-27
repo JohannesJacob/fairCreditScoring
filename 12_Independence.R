d5a5023d0058e100d53b54cd0aa61b39df4bfd5a
JohannesJacob <johannes_jacob@gmx.de>
2020-01-27 10:08:07 +0100

    python code for NN to learn fair representations

--- 12_Independence.R
+++ 12_Independence.R
@@ -3,6 +3,14 @@
 library('fairness')
 
 # Preprocessing
+
+# OTT
+
+# Postprocessing based on final classification (not their propabilities)
 data(compas)
-dem_parity(data = compas, group ='ethnicity',probs ='probability', preds = NULL,cutoff = 0.4, base ='Caucasian')
-dem_parity(data = compas, group ='ethnicity',probs = NULL, preds ='predicted',cutoff = 0.5, base ='Hispanic')
+indep <- dem_parity(data = compas, group ='ethnicity',probs = NULL, preds ='predicted',cutoff = 0.5, base ='Caucasian')
+
+
+while (condition) {
+  
+}
